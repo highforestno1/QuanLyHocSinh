650225ba974417a616eedcb8551b41e337fa71d3
CAOLAM <vucaolam19@gmail.com>
2023-03-03 11:34:32 +0700

    studentDto

--- src/QLHS.Application.Contracts/Teachers/CreateUpdateTeacherDto.cs
+++ src/QLHS.Application.Contracts/Teachers/CreateUpdateTeacherDto.cs
@@ -9,4 +9,6 @@ public class CreateUpdateTeacherDto
     public DateTime BirthDate { get; set; }
         
     public string ShortBio { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
 }
--- src/QLHS.Application.Contracts/Teachers/TeacherDto.cs
+++ src/QLHS.Application.Contracts/Teachers/TeacherDto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Globalization;
 using Volo.Abp.Application.Dtos;
 
 namespace QLHS.Teachers;
 
-public class TeacherDto:EntityDto<Guid>
+public class TeacherDto : EntityDto<Guid>
 {
     public string Name { get; set; }
-        
+
     public DateTime BirthDate { get; set; }
-        
+
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
     public string ShortBio { get; set; }
 }
